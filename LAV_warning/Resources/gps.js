3dba051afff1124ad072b47fffb1a4e1c0ba5991
Danilo Tomasoni <danilo.tomasoni@cryptolab.net>
2012-01-22 16:49:20 +0100

    added gpl license

--- LAV_warning/Resources/gps.js
+++ LAV_warning/Resources/gps.js
@@ -1,3 +1,16 @@
+/* "LAV Warning" is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/*   (at your option) any later version. */
+  
+/*   "LAV Warning" is distributed in the hope that it will be useful, */
+/*   but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/*   GNU General Public License for more details. */
+  
+/*   You should have received a copy of the GNU General Public License */
+/*   along with "LAV Warning".  If not, see <http://www.gnu.org/licenses/>. */
+
 Ti.Geolocation.purpose = 'Get Current Location';
 Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
 Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
